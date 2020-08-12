00a22e61e086014dfcb12925d1a47e749b4ee3b5
Jarren <jarrenmcampos@gmail.com>
2020-08-11 19:41:55 -0700

    added Project

---
+++ Contacts Mrc/Contacts Mrc/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Contacts Mrc
+//
+//  Created by Jarren Campos on 8/11/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Contacts Mrc/Contacts Mrc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Contacts Mrc
+//
+//  Created by Jarren Campos on 8/11/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
